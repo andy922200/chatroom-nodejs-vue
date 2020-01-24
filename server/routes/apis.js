fe37527876f354449f31148558a1eeb2ab67273d
andysml <andy922200@gmail.com>
2020-01-24 13:06:10 +0800

    [backend] add basic routes

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-
+router.use('/public-chatroom', (req, res) => {
+  res.send('This is "public-chatroom" page.')
+})
+//router.get('/public-chatroom',)
 
 module.exports = router
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,4 +2,7 @@ const apis = require('./apis')
 
 module.exports = (app) => {
   app.use('/api', apis)
+  app.use('/', (req, res) => {
+    res.send('This is "/" page (temporary). === 404')
+  })
 }
