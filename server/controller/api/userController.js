666e11cde2363216338380951c32cab3d622127a
andysml <andy922200@gmail.com>
2020-01-26 01:34:29 +0800

    [backend] add passport authentication & JWT-token

--- server/controller/api/userController.js
+++ server/controller/api/userController.js
@@ -1,16 +1,34 @@
 const db = require('../../models')
-const Sequelize = require('sequelize')
 const User = db.User
-const Message = db.Message
-const Op = Sequelize.Op
+// for signIn authentication
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 let userController = {
   signIn: (req, res) => {
-    /*User.findOne({ where: { email: username } })
-      .then(user => {
-        console.log(user)
-      })*/
-    res.send('success to log signin')
+    let username = req.body.email
+    let password = req.body.password
+    User.findOne({
+      where: { email: username }
+    }).then(user => {
+      if (!user) return res.status(401).json({ status: 'error', message: 'No user was found.' })
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ status: 'error', message: 'The password did not match.' })
+      }
+      var payload = { id: user.id }
+      var token = jwt.sign(payload, process.env.JWT_SECRET)
+      return res.json({
+        status: 'success',
+        message: 'OK',
+        token: token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin
+        }
+      })
+    })
   }
 }
 
--- server/routes/apis.js
+++ server/routes/apis.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const userController = require('../controller/api/userController')
 
-router.get('/public-chatroom', userController.signIn)
+// for authentication
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+router.post('/signIn', userController.signIn)
 
 module.exports = router
