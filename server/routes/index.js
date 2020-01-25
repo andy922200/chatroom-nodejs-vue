6866ea1b01e88c4c6b99a0539998ac1c619cf4cc
andysml <andy922200@gmail.com>
2020-01-26 01:42:10 +0800

    [backend] return "no result" for '*'

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,6 @@ const apis = require('./apis')
 module.exports = (app) => {
   app.use('/api', apis)
   app.use('/', (req, res) => {
-    res.send('This is "/" page (temporary). === 404')
+    return res.json({ status: 'error', message: 'no result' })
   })
 }
