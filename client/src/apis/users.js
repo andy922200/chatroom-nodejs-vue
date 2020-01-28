bccebad0d64cf184a1fa657ac91b6d851d876c13
andysml <andy922200@gmail.com>
2020-01-29 01:29:49 +0800

    [frontend] add store-state,mutations,actions

--- client/src/apis/users.js
+++ client/src/apis/users.js
@@ -1,3 +1,16 @@
 // ./src/apis/users.js
 import { apiHelper } from './../utils/helpers'
 const getToken = () => localStorage.getItem('token')
+
+export default {
+  getCurrentUser() {
+    return apiHelper.get(`/get_current_user`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+  getUser({ userId }) {
+    return apiHelper.get(`/users/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  }
+}
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import usersAPI from '../apis/users'
 
 Vue.use(Vuex)
 
@@ -23,7 +24,25 @@ export default new Vuex.Store({
     }
   },
   actions: {
-
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data, statusText } = await usersAPI.getCurrentUser()
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+        // change the state by commit and the response
+        commit('setCurrentUser', {
+          id: data.user.id,
+          name: data.user.name,
+          email: data.user.email,
+          isAdmin: data.user.isAdmin
+        })
+        return true
+      } catch (err) {
+        console.log('error', err)
+        return false
+      }
+    }
   },
   modules: {
   }
