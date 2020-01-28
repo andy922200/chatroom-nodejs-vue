d9bd610b4819546511d8f9d174bff4ada3f2077c
andysml <andy922200@gmail.com>
2020-01-29 07:41:58 +0800

    [frontend] import vue-font-awesome

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// intialize vue-font-awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+library.add(faGithub)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
