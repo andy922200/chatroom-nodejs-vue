6aa7ddce574840a48d3f598e60ca5ce5a94c0f93
andysml <andy922200@gmail.com>
2020-02-03 02:54:02 +0800

    [frontend] add moment.js

---
+++ client/src/utils/mixins.js
@@ -0,0 +1,10 @@
+// ./src/utils/mixins.js
+import moment from 'moment'
+
+export const fromNowFilter = {
+  filters: {
+    fromNow(datetime) {
+      return datetime ? moment(datetime).fromNow() : '-'
+    }
+  }
+}
