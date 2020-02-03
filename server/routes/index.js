0c7f96397afb654039f1f0c44beb23de0e1a0bcd
andysml <andy922200@gmail.com>
2020-02-03 10:34:49 +0800

    [heroku] fix url at server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const apis = require('./apis')
 
 module.exports = (app) => {
   app.use('/api', apis)
-  app.use('/', (req, res) => {
+  /*app.use('/', (req, res) => {
     return res.json({ status: 'error', message: 'no result' })
-  })
+  })*/
 }
